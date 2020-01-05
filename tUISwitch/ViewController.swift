b7b5ac6f16f89ffaad669c2661aa42cb28c6b4b9
tyobigoro <n_nakano@mbm.nifty.com>
2020-01-05 21:45:24 +0900

    c

--- tUISwitch/ViewController.swift
+++ tUISwitch/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tSwitch: UISwitch!
+    
+    var switchStatus: Bool = true {
+        didSet {
+            if oldValue != switchStatus {
+                saveSwitchStatus()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        readSwitchStatus()
     }
-
-
+    
+    @IBAction func switchDidTap(_ sender: UISwitch) {
+        switchStatus = sender.isOn
+    }
+    
+    @IBAction func transitionBtnDidTap(_ sender: Any) {
+        performSegue(withIdentifier: "Goto2ndVC", sender: nil)
+    }
+    
+    @IBAction func backFromSecondVC(segue: UIStoryboardSegue){
+        print("backFrom2ndVC")
+    }
+    
+    func saveSwitchStatus() {
+        let ud = UserDefaults.standard
+        ud.set(switchStatus, forKey: "switchStatus")
+    }
+    
+    func readSwitchStatus() {
+        let ud = UserDefaults.standard
+        ud.register(defaults: ["switchStatus": true])
+        switchStatus = ud.bool(forKey: "switchStatus")
+        tSwitch.isOn = switchStatus
+    }
+    
 }
 
+class SeondVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func pushReturnBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
